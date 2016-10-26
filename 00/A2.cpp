c146e6a9c9f7f076b125b49c5b545cae51483325
Letitia Parcalabescu <leti_parcalabescu@yahoo.com>
2016-10-26 18:58:36 +0200

    Second exercise

---
+++ 00/A2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+float f(float a, int n)
+{
+	float f0 = 0.5;
+
+	for(int i = 0; i < n; i++)
+		f0 = a * f0 * (1 - f0);
+
+	return f0;
+}
+
+int main () 
+{
+	float a = 2.0;
+	while(a < 3.9)
+	{
+		for(int n = 100; n < 105; n++)
+		{
+			cout << "For a = "<< a << " we have f(" << n << ") = " << f(a, n) << "\n";
+		}
+		a += 0.1;
+	}
+}
