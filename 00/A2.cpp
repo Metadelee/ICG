81dd682a47621d1b128d81001adfe71e33f17a81
Letitia Parcalabescu <leti_parcalabescu@yahoo.com>
2016-10-27 16:27:31 +0200

    Adjust indentation

--- 00/A2.cpp
+++ 00/A2.cpp
@@ -4,23 +4,23 @@ using namespace std;
 
 float f(float a, int n)
 {
-	float f0 = 0.5;
+    float f0 = 0.5;
 
-	for(int i = 0; i < n; i++)
-		f0 = a * f0 * (1 - f0);
+    for(int i = 0; i < n; i++)
+        f0 = a * f0 * (1 - f0);
 
-	return f0;
+    return f0;
 }
 
 int main () 
 {
-	float a = 2.0;
-	while(a < 3.9)
-	{
-		for(int n = 100; n < 105; n++)
-		{
-			cout << "For a = "<< a << " we have f(" << n << ") = " << f(a, n) << "\n";
-		}
-		a += 0.1;
-	}
+    float a = 2.0;
+    while(a < 3.9)
+    {
+        for(int n = 100; n < 105; n++)
+        {
+            cout << "For a = "<< a << " we have f(" << n << ") = " << f(a, n) << "\n";
+        }
+        a += 0.1;
+    }
 }
