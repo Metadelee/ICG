671f8e736b3034a9c735235e1c1c0cc122d74198
Metadelee <lina.gundelwein@yahoo.de>
2016-11-20 20:13:53 +0100

    calling HitTest

--- 04/Skelett/src/Raytracer/Objects/Sphere.cpp
+++ 04/Skelett/src/Raytracer/Objects/Sphere.cpp
@@ -34,7 +34,9 @@ void Sphere::GetIntersection(const Ray &ray, float distance, Intersection &inter
 	// d.h. füllen sie alle Werte der struct intersection.
     	// Der float-Wert distance enthält den Abstand Ursprung des Rays - Schnittpunkt, die ray Variable
     	// alle Informationen des gecasteten Strahls (siehe Ray-Klasse).
-	intersection.position = ray.GetOrigin() + ray.GetDirection();
+	RayHit hit;
+	if(Sphere::HitTest(ray, hit)) std::cout<<"test 11\n";
+	intersection.position = hit.GetDistance();
 	intersection.material = this->material;
 	intersection.normal = normalize(this->center - intersection.position); 
 	intersection.viewDirection = intersection.position-float3(0.0f, 0.0f, 15.0f);
@@ -46,12 +48,16 @@ bool Sphere::HitTest(const Ray &ray, RayHit &hit) const
 {
 	//TODO: Somehow I beleive this is too complicated...
 	// Took this from http://www.scratchapixel.com/lessons/3d-basic-rendering/minimal-ray-tracer-rendering-simple-shapes/ray-sphere-intersection
+	
 	float x0,x1;
 	float a = length(ray.GetDirection()*ray.GetDirection());
 	float b = 2.*length(ray.GetDirection()*(ray.GetOrigin()-this->center));
 	float c = length((ray.GetOrigin()-this->center)*(ray.GetOrigin()-this->center))-this->radius2;
  	float discr = b * b - 4 * a * c;
-	if(discr < 0) return false;
+	if(discr < 0) {
+		//std::cout<<a << ", "<< b <<", " << c << "\n";
+		return false;
+	}
 	else if(discr == 0) x0 = x1 = - 0.5 * b / a;
 	else {
 		float q = (b > 0) ?
