0ffad6a9dd4abf98860eab6429bf11d586d57c98
LetiP <leti_parcalabescu@yahoo.com>
2016-11-20 20:36:40 +0100

    Hallelujaah!! Works! Beautify indentation

--- 04/Skelett/src/Raytracer/Objects/Sphere.cpp
+++ 04/Skelett/src/Raytracer/Objects/Sphere.cpp
@@ -65,8 +65,8 @@ bool Sphere::HitTest(const Ray &ray, RayHit &hit) const
 	else if (discr == 0) x0 = x1 = -0.5 * b / a;
 	else {
 		float q = (b > 0) ?
-			-0.5 * (b + sqrt(discr)) :
-			-0.5 * (b - sqrt(discr));
+		-0.5 * (b + sqrt(discr)) :
+		-0.5 * (b - sqrt(discr));
 		x0 = q / a;
 		x1 = c / q;
 	}
