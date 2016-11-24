635b5573e8030a10778a67fd14def8c31ef45f41
Metadelee <lina.gundelwein@yahoo.de>
2016-11-24 16:26:36 +0100

    first commit

---
+++ 05/Skeleton/src/Raytracer/Scenes/PointLight.cpp
@@ -0,0 +1,33 @@
+#include <Raytracer/Raytracer.h>
+#include<iostream>
+#include<cmath>
+using namespace Raytracer;
+using namespace Raytracer::Scenes;
+
+PointLight::PointLight(const float3 &position, const float3 &intensity)
+{
+  this->position = position;
+  this->intensity = intensity;
+}
+
+float3 PointLight::ComputeDirectContribution(const Intersection &intersection, const Scene &scene)
+{
+  // TODO: Implement the calculation of the diffuse and specular lighting according to the
+  // Phong light model at the point determined by the intersection calculation.
+	float3 I = intersection.material->GetAmbient()*intensity;	
+	float3 L = this->position-intersection.position;
+
+	float NdotL = dot(intersection.normal, L);
+	if (NdotL >0){
+		I += intersection.material->GetDiffuse()*intensity*NdotL;
+		
+		float3 R = (2.0f*intersection.normal*NdotL)-L;
+		float RdotV = dot(R,intersection.viewDirection);
+		if (RdotV > 0){
+			I+= intersection.material->GetSpecular()*intensity*powf(RdotV, intersection.material->GetShininess());
+		}
+	}
+  // Also respect that the intensity of the light will decrease with increasing distance to the light source,
+  // or by positioning them behind other objects (shadowing).
+  return I;
+}
